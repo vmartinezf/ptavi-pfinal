b2746a028bf57fbfaf57d593a6f557339320847c
vmartinezf <vanessa_1895@msn.com>
2016-01-02 20:41:53 +0100

    uaserver.py con procedimiento principal con los valores del contenido de xml

--- uaserver.py
+++ uaserver.py
@@ -29,7 +29,7 @@ class XMLHandler(ContentHandler):
                     'rtpaudio': ['puerto'],
                     'regproxy': ['ip', 'puerto'],
                     'log': ['path'],
-					'audio': ['path']}
+                    'audio': ['path']}
 
     def startElement(self, name, attrs):
         """
@@ -63,11 +63,11 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             line = self.rfile.read()
             line_decod = line.decode('utf-8')
             METHOD = line_decod.split(' ')[0].upper()
-            METHODS = ['INVITE', 'BYE', 'ACK']
+            METHODS = ['INVITE', 'BYE', 'ACK', 'REGISTER']
             if len(line_decod) >= 2:
-				if METHOD == 'REGISTER':
-					# FALTA POR HACER
-                if METHOD == 'INVITE':
+                if METHOD == 'REGISTER':
+                    # FALTA POR HACER
+                elif METHOD == 'INVITE':
                     message_send = b'SIP/2.0 100 Trying\r\n\r\n'
                     message_send += b'SIP/2.0 180 Ring\r\n\r\n'
                     message_send += b'SIP/2.0 200 OK\r\n\r\n'
@@ -92,7 +92,38 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     try:
+		# Archivo xml pasado como parámetro
         CONFIG = sys.argv[1]
+
+        # Comprobación de si existe el fichero pasado como parámetro
+        if os.path.exists(CONFIG) is False:
+        	print ("This name of file doesn´t exist")
+            raise SystemExit
+
+		# Saco el contenido del fichero xml
+		parser = make_parser()
+    	cHandler = SmallSMILHandler()
+    	parser.setContentHandler(cHandler)
+    	parser.parse(open(CONFIG))
+		lista = cHandler.get_tags()
+
+        diccionario = lista[0]
+        # Meto los valores del xml en variables
+        USERNAME = diccionario['account']['username']
+        PASSWD = diccionario['account']['passwd']
+        UASERVER_IP = diccionario['uaserver']['ip']
+        UASERVER_PORT = diccionario['uaserver']['puerto']
+        PORTAUDIO  = diccionario['rtpaudio']['puerto']
+        IPPROXY = diccionario['regproxy']['ip']
+        PORTPROXY = diccionario['regproxy']['puerto']
+        PATHLOG = diccionario['log']['path']
+        PATHAUDIO = diccionario['audio']['path']
+
+        # Comprobación de si existe el archivo de audio
+        if not os.path.exists(PATHAUDIO):
+           print "This name of file os audio doesn´t exist"
+           raise SystemExit    
+
     except IOError:
         sys.exit("Usage: python uaserver.py config")
     serv = socketserver.UDPServer(('', PORT), EchoHandler)
